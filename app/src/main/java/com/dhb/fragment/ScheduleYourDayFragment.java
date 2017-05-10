b00dbfc9ce64663a70044e8803ed7755db7092a5
Jayesh Yeola <jayesh@digitalhealthbook.in>
2017-05-10 11:42:20 +0530

    Added Icons for Slots screen Updated Slots screen Changes from tester UI improvisations

--- app/src/main/java/com/dhb/fragment/ScheduleYourDayFragment.java
+++ app/src/main/java/com/dhb/fragment/ScheduleYourDayFragment.java
@@ -106,7 +106,7 @@ public class ScheduleYourDayFragment extends AbstractFragment {
                 btnProceed.setVisibility(View.INVISIBLE);
                 isAvailable = false;
                 AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                builder.setMessage("Are you sure that you will not be able to work tomorrow ?")
+                builder.setMessage("Are you sure that you are not available tomorrow ?")
                         .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int id) {
